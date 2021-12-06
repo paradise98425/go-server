d3d9b8127d650c7be34146fd293ca03cd1075c8b
paradise98425 <paradise98425@gmail.com>
2021-12-06 21:32:45 +0100

    changes

--- database/config.js
+++ database/config.js
@@ -4,7 +4,7 @@ const pool = createPool({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Password123!',
+    password: 'password',
     database: 'go_explore',
     connectionLimit: 10
 });
